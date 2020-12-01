2788e74fad66245d1bd62a3e91a85173fcccad9e
Sergio Andrés Díaz Ariza <69307962+CHEJIN95@users.noreply.github.com>
2020-12-01 18:13:57 -0500

    Sercurity with Allow, Iron:router and organize templates and global temp

--- client/main.js
+++ client/main.js
@@ -1,8 +1,65 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session';
+Images = new Mongo.Collection("images");
 
 import './main.html';
+//Global Router
+Router.configure({
+  layoutTemplate:'ApplicationLayout'
+});
 
+//Router of thte template
+Router.route('/', function () {
+  this.render('regards',{
+    to:'main'
+  });
+});
+
+Router.route('/images', function () {
+  this.render('navbar',{
+    to:'navbar'
+  });
+  this.render('images',{
+    to:'main'
+  });
+});
+
+Router.route('/image/:_id', function () {
+  this.render('navbar',{
+    to:'navbar'
+  });
+  this.render('image',{
+    to:'main',
+    data:function(){
+      return Images.findOne({_id:this.params._id})
+    }
+  });
+});
+
+//Scroll event
+Session.set("imageLimit", 8);
+
+lastScrollTop = 0;
+$(window).scroll(function(event){
+  // test if we are near the bottom of the window
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    // where are we in the page?
+    var scrollTop = $(this).scrollTop();
+    // test if we are going down
+    if (scrollTop > lastScrollTop){
+      // yes we are heading down...
+     Session.set("imageLimit", Session.get("imageLimit") + 4);
+    }
+
+    lastScrollTop = scrollTop;
+  }
+
+});
+
+//end scroll event
+
+//this come with meteor by default
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
@@ -21,9 +78,9 @@ Template.hello.events({
   },
 });
 
-if (Meteor.isClient) {
-  console.log('I am in the Client');
-  var img_data = [
+//if (Meteor.isClient) {
+
+var img_data = [
     {
     img_src:"laptops.jpg",
     img_alt:"lots of laptops screen"
@@ -36,13 +93,112 @@ if (Meteor.isClient) {
     img_src:"beard.jpg",
     img_alt:"musician with beard"
     },
-  ];
-  Template.images.helpers({images:img_data});
+];
+//}
+console.log('I am in the Client');
 
-  Template.images.events({
-    'click .js-image':function(event){
-        $(event.target).css("width","75px");
+//add image from user
+Template.image_add_form.events({
+  'submit .js-add-image':function(event){
+    var img_src, img_alt;
+    img_src = event.target.img_src.value;
+    img_alt = event.target.img_alt.value;
+    console.log("src:"+img_src+" alt:"+img_alt);
+    if (Meteor.user()) {
+      Images.insert({
+        img_src: img_src,
+        img_alt: img_alt,
+        createdOn: new Date(),
+        createdBy: Meteor.user()._id
+      })
     }
-  });
+    return false;
+  }
+});
 
-}
+//sort images by user and Rating
+Template.images.helpers({
+  images:function(){
+    if (Session.get("userFilter")){// they set a filter!
+      return Images.find({createdBy:Session.get("userFilter")}, {sort:{createdOn: -1, rating:-1}});
+    }
+    else {
+      return Images.find({}, {sort:{createdOn: -1, rating:-1}, limit:Session.get("imageLimit")});
+    }
+  },
+  filtering_images:function(){
+    if (Session.get("userFilter")) {
+      return true;
+      }
+    else {
+      return false;
+    }
+  },
+  getFilterUser:function(){
+    if (Session.get("userFilter")) {
+      var user = Meteor.users.findOne({_id:Session.get("userFilter")});
+      return user.username;
+    }
+    else {
+      return false;
+    }
+  },
+  getUser:function(user_id){
+    var user = Meteor.users.findOne({_id:user_id});
+    if (user) {
+      return user.username;
+      }
+    else {
+      return 'anon';
+      }
+    }
+});
+
+//set Accounts with accounts package
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
+
+//Name of the User that into in the page
+Template.body.helpers({username:function(){
+  if (Meteor.user()){
+    return Meteor.user().username;
+    //return Meteor.user().emails[0].address;
+  }
+  else {
+    return "anonymus internet user";
+  }
+  }
+});
+
+// events of mages Ratng-size-remove-update
+Template.images.events({
+  'click .js-image':function(event){
+      $(event.target).css("width","75px");
+  },
+  'click .js-del-image':function(event){
+    var image_id = this._id;
+    console.log(image_id);
+    $('#'+image_id).hide('slow',function(){
+      Images.remove({'_id':image_id});
+    })
+  },
+  'click .js-rate-image':function(event){
+    var rating = $(event.currentTarget).data('userrating');
+    console.log(rating);
+    var image_id = this.id;
+    console.log(image_id);
+
+    Images.update({_id:image_id},{$set:{rating:rating}})
+  },
+  'click .js-show-image-form':function(event){
+    $('#image_add_form').modal('show');
+  },
+  'click .js-set-image-user-filter':function(event){
+    Session.set("userFilter",this.createdBy);
+  },
+  'click .js-unset-image-user-filter':function(event){
+    Session.set("userFilter",undefined);
+  }
+
+});
--- server/main.js
+++ server/main.js
@@ -1,6 +1,54 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { Session } from 'meteor/session';
+
+Images = new Mongo.Collection("images");
+
+console.log('main.js Server say: '+Images.find().count());
+
+console.log('main.js Server say: '+"I am the Server");
 
 Meteor.startup(() => {
-  console.log("I am the Server");
+  if (Images.find().count()==0) {
+    for (var i = 1; i < 23; i++) {
+      Images.insert(
+        {
+        img_src:"img_"+i+".jpg",
+        img_alt:"image number"+i
+        }
+      );
+    }; //End cycle for 'insert images'
+  };
+  //console.log(Images.find().count());
   // code to run on server at startup
+});
+
+// try to put something of secure
+Images.allow({
+  insert:function(userId,doc) {
+    if (Meteor.user()) { //they are logged in
+      if (userId !=doc.createdBy) { //the user is messing
+        return false;
+      }
+      else {//the user is correct
+        return true;
+      }
+    }
+    else { //the user isn't logged in
+      return false;
+    }
+  },
+  remove:function(userId,doc) {
+    if (Meteor.user()) { //they are logged in
+      if (userId !=doc.createdBy) { //the user is messing
+        return false;
+      }
+      else {//the user is correct
+        return true;
+      }
+    }
+    else { //the user isn't logged in
+      return false;
+    }
+  }
 });
